a9dee03854a9e501942a4e1aaa091f3bfe0bbb85
Edouard Leroy <leroyedouard@protonmail.com>
2024-01-11 16:07:37 +0100

    Servlet4

---
+++ src/main/java/com/directmedia/onlinestore/backoffice/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.directmedia.onlinestore.backoffice;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/home")
+public class HomeServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>OnlineStore - Gestion de votre boutique</h1></body></html>");
+    }
+}
