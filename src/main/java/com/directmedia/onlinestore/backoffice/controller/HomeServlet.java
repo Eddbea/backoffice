b907a79affa9a3a6aad824da50eba22a4b817f89
Edouard Leroy <leroyedouard@protonmail.com>
2024-01-16 17:08:29 +0100

    exercice termine

--- src/main/java/com/directmedia/onlinestore/backoffice/controller/HomeServlet.java
+++ src/main/java/com/directmedia/onlinestore/backoffice/controller/HomeServlet.java
@@ -1,4 +1,4 @@
-package com.directmedia.onlinestore.backoffice;
+package com.directmedia.onlinestore.backoffice.controller;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +13,8 @@ public class HomeServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html><body><h1>OnlineStore - Gestion de votre boutique</h1></body></html>");
+        out.println("<html><body><h1>OnlineStore - Gestion de la boutique</h1></body></html>");
+        out.println("<a href=\"catalogue\">Accès au catalogue</a>");
+        out.println("</body></html>");
     }
 }
